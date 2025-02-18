c61343df981b00825379bb844b0bb3654d7b5cde
Ankithareddy98 <ankitha.reddy533@gmail.com>
2025-02-18 13:35:11 -0500

    created Hello WOrld! block

---
+++ web/modules/custom/test_module/src/Plugin/Block/HelloBlock.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Drupal\test_module\Plugin\Block;
+
+use Drupal\Core\Block\Attribute\Block;
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+
+/**
+ * Provides a 'Hello' Block.
+ */
+
+ #[Block(
+    id: "hello_block",
+    admin_label: new TranslatableMarkup("Hello block"),
+    category: new TranslatableMarkup("Hello World")
+  )]
+
+  class HelloBlock extends BlockBase {
+
+    /**
+     * {@inheritdoc}
+     */
+   public function build() {
+    return [
+        '#markup' => $this->t('Hello, World!'),
+    ];
+   }
+  }
+  
