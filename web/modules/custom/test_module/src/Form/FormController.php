6b0bf5e01f251d500e80734579af0c2037d65941
Ankithareddy98 <ankitha.reddy533@gmail.com>
2025-03-12 16:32:05 -0400

    Printing tasks on Form using AJAX

--- web/modules/custom/test_module/src/Form/FormController.php
+++ web/modules/custom/test_module/src/Form/FormController.php
@@ -4,6 +4,7 @@ namespace Drupal\test_module\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Render\Markup;
 
 /**
  * Provides ToDo Form.
@@ -21,6 +22,11 @@ class FormController extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
+ // Store previous inputs in form state if not already set
+    if (!$form_state->get('inputs')) {
+      $form_state->set('inputs', []);
+    }
+
     // Input text field to add task.
     $form['input'] = [
         '#type' => 'textfield',
@@ -32,7 +38,33 @@ class FormController extends FormBase {
     $form['submit'] = [
         '#type' => 'submit',
         '#value' => $this->t('ADD'),
+        '#ajax' => [
+        'callback' => '::myAjaxCallback',
+        'event' => 'click',
+        'disable-refocus' => FALSE,
+        'wrapper' => 'task-list-wrapper',
+        'progress' => [
+          'type' => 'throbber',
+          'message' => $this->t('Adding task...'),
+        ],
+      ],
     ];
+
+    // Task List Wrapper (This will be updated via AJAX)
+    $form['task_list'] = [
+      '#type' => 'container',
+      '#attributes' => ['id' => 'task-list-wrapper'],
+    ];
+
+    // Retrieve saved inputs
+    $inputs = $form_state->get('inputs');
+    if (!empty($inputs)) {
+      $form['task_list']['inputs'] = [
+        '#theme' => 'item_list',
+        '#items' => $inputs,
+        '#title' => $this->t('Task List'),
+      ];
+    }
 
     return $form;
   }  
@@ -42,6 +74,21 @@ class FormController extends FormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     // Display Task here.
-    $this->messenger()->addStatus($this->t('@task', ['@task' => $form_state->getValue('input')]));
+   // $this->messenger()->addStatus($this->t('@task', ['@task' => $form_state->getValue('input')]));
+   $input_value = $form_state->getValue('input');
+
+   $inputs = $form_state->get('inputs');
+   $inputs[] = $input_value;
+   $form_state->set('inputs', $inputs);
+
+   $form_state->setValue('input', '');
+
+   $form_state->setRebuild(TRUE);
   }
+
+  public function myAjaxCallback(array &$form, FormStateInterface $form_state) {
+    return $form['task_list']; // Return the updated task list container
+  }
+  
 }
+
